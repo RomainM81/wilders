be304faf1ac043a8ac0479d88e66403ded29d12c
RomainM81 <dcromain112@gmail.com>
2021-06-16 14:38:25 +0100

    create wilders

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('Dorothée'));
+console.log(chalk.blue('Tristan'));
+console.log(chalk.cyan('Ludivine'));
+console.log(chalk.red('Remy'));
+console.log(chalk.white('Benoit'));
+console.log(chalk.yellowBright('Benjamin'));
